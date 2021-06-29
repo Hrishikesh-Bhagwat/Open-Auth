b8a1c73a891fad48191c6255baaa78e3060ffc64
Hrishikesh-Bhagwat <bhagwatcoder@gmail.com>
2021-06-29 16:21:22 +0530

    Enabled CORS

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express=require('express')
-
+const cors=require('cors')
 const app=express()
 
 const PORT=3000;
 
 app.use(express.json())
-
+app.use(cors())
 app.get("/device-status",function(req,res){
     return res.sendStatus(200);
 })
